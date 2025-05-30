9a6da8cc186232ddd942b5481febf621a142f41d
cehayford <sean.alpha18@gmail.com>
2025-05-30 10:10:45 -0500

    commit code from cloud9

---
+++ collections.py
@@ -0,0 +1,40 @@
+# list
+myFruitList = ["apple", "banana", "cherry"]
+print(myFruitList)
+print(type(myFruitList))
+
+print(myFruitList[0])
+print(myFruitList[1])
+print(myFruitList[2])
+
+# changing the calues in a list
+myFruitList[2] = "orange"
+print(myFruitList)
+
+
+
+#tuple
+myFinalAnswerTuple = ("apple", "banana", "pineapple")
+print(myFinalAnswerTuple)
+print(type(myFinalAnswerTuple))
+
+print(myFinalAnswerTuple[0])
+print(myFinalAnswerTuple[1])
+print(myFinalAnswerTuple[2])
+
+
+
+#Dictionary
+myFavoriteFruitDictionary = {
+  "Akua" : "apple",
+  "Saanvi" : "banana",
+  "Paulo" : "pineapple"
+}
+print(myFavoriteFruitDictionary)
+print(type(myFavoriteFruitDictionary9))
+
+
+print(myFavoriteFruitDictionary["Akua"])
+print(myFavoriteFruitDictionary["Saanvi"])
+print(myFavoriteFruitDictionary["Paulo"])
+
