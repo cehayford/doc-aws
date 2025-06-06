57d257d6a106eb1c1d3bfb3ab2c3e02df84f3ae6
cehayford <sean.alpha18@gmail.com>
2025-06-06 11:19:30 -0500

    add challenge_solve_171.sh script for setting up HTTP server and creating HTML file

---
+++ challenge_solve_171.sh
@@ -0,0 +1,12 @@
+aws configure set default.region us-west-2
+
+# how to install httpd
+sudo yum install httpd -y
+sudo systemctl start httpd
+
+cd /var/www/html
+
+# creating html file with vi
+sudo vi projects.html
+
+
