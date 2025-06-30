6f68c820e894d680a2d6ba127a375b57abf6b838
cehayford <sean.alpha18@gmail.com>
2025-06-30 11:18:38 -0500

    Refactor CloudFormation scripts: add EC2 instance management commands and remove obsolete trail.sh

--- Tic-Out/challenge_cloudformation.sh
+++ Tic-Out/challenge_cloudformation.sh
@@ -2,4 +2,25 @@
 # create an AWS VPC and Amazon EC2 instance
 
 
-#!/bin/bash
+# upload the yaml file to cloudformation
+# check the file upload.yaml in the current directory
+# Re-QQQ
+# Re-QQQ
+# Re-QQQ
+# Re-QQQ
+# Re-QQQ
+
+# return the account number and your user ID:
+aws sts get-caller-identity
+
+# checking for the instance description
+aws ec2 describe-instances
+
+
+# return the all information about the ec2 instance
+# use the py command to check the instance
+$ python3
+>>> import boto3
+>>> ec2 = boto3.client('ec2', region_name='us-west-2')  
+>>> ec2.describe_regions()
+>>> exit()
