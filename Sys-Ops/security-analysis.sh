883fa84afbb0d444afe6f2e8624c6e5ec4bee76f
cehayford <sean.alpha18@gmail.com>
2025-09-08 09:17:09 -0500

    AWS SDK with python

--- Sys-Ops/security-analysis.sh
+++
@@ -1,2 +0,0 @@
-# increases the CPU load of the EC2 instance.
-sudo stress --cpu 8 --timeout 300s
--- Tooling&Auto-Scaling/automation.sh
+++
@@ -1,9 +0,0 @@
-# Get region
-AZ=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone`
-export AWS_DEFAULT_REGION=${AZ::-1}
-
-
-# List information about EC2 instances
-aws ec2 describe-instances
-aws ec2 dwscribe-instance-types
-aws ec2 describe-instance-status
